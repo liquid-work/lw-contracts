e15a9d36ead02a0628ea9f7759c980225e3b223d
Blinc <buta.cristi14@gmail.com>
2022-05-22 00:07:25 +0300

    scripts for testing user flow

--- scripts/stopFlow.js
+++ scripts/stopFlow.js
@@ -1,20 +1,10 @@
 const { Framework } = require("@superfluid-finance/sdk-core");
-const { BigNumber } = require("ethers");
 const { ethers } = require("hardhat");
-const ABI_LW =
-  require("../artifacts/contracts/SuperLiquidWork.sol/SuperLiquidWork.json").abi;
-const ABI_MATICX = require("../abi/MATICX");
 
 const { PRIVATE_KEY, ALCHEMY_API_URL_KEY, NETWORK_NAME } = process.env;
 const networkConfigs = require("../network.configs.js");
 
-const createFlow = async (
-  ourContractAddress,
-  deposit,
-  flowRate,
-  instanceId,
-  userData
-) => {
+const stopFlow = async (ourContractAddress, userData) => {
   const ethersProvider = new ethers.providers.JsonRpcProvider(
     ALCHEMY_API_URL_KEY,
     {
@@ -33,34 +23,23 @@ const createFlow = async (
     provider: ethersProvider,
   });
 
-  const lwContract = new ethers.Contract(
-    ourContractAddress,
-    ABI_LW,
-    superSigner
-  );
-  await lwContract.deposit(instanceId, {
-    value: deposit,
-  });
-
   const options = {
+    sender: await superSigner.getAddress(),
     receiver: ourContractAddress,
     superToken: networkConfigs[NETWORK_NAME].maticx,
-    flowRate: flowRate,
     userData,
   };
-  const createFlowOperation = sf.cfaV1.createFlow(options);
+  const createFlowOperation = sf.cfaV1.deleteFlow(options);
 
   await createFlowOperation.exec(superSigner);
 };
 
 const ourContractAddress = "0x5098FfE333036bf8038D850d3D2F925eDF915110";
-const deposit = ethers.utils.parseEther("0.00002");
-const flowRate = "100";
 const instanceId = "testInstanceId";
 const userData = web3.eth.abi.encodeParameter("string", instanceId);
 
 try {
-  createFlow(ourContractAddress, deposit, flowRate, instanceId, userData);
+  stopFlow(ourContractAddress, userData);
 } catch (error) {
   console.log("Error:", error);
 }
