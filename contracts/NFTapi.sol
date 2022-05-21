9419c6f432cc3e609a041d295ac66e0c99e338f2
mrhouzlane <mrhouzlane@github.com>
2022-05-21 01:39:47 -0400

    chainlinkAPI

--- contracts/NFTapi.sol
+++ contracts/NFTapi.sol
@@ -3,42 +3,22 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-abstract contract NFTExp is ChainlinkClient, ERC721 {
+contract APILiquidWork is ChainlinkClient, ERC721URIStorage {
     using Chainlink for Chainlink.Request;
-    //using SafeMath for uint256; 
 
+    uint256 public realTime; 
 
-    bytes32 public timeStarted; 
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
+    // When TimeLeft == 0 emit this Event
     event ExperienceFinished(bytes32 firstName, bytes32 lastName, uint256 TimeLeft);
-
-
-    struct Status{
-        uint256 one; //started 
-        uint256 two; //finished 
-    }
-
-    struct Experience{
-        Status status; 
-        string review;
-
-    }
-
-    Experience[] public experiences;
-
-    mapping(bytes32 => uint256) requestToTokenId; 
-    mapping(bytes32 => bytes32) requestToNewTime; 
-    mapping(bytes32 => address) requestToSender;
-    bytes32 public requestId; 
- 
-
 
     /**
      * Network: Kovan
@@ -47,7 +27,7 @@ abstract contract NFTExp is ChainlinkClient, ERC721 {
      * Job ID: d5270d1c311941d0b08bead21fea7747
      * Fee: 0.1 LINK
      */
-    constructor() ERC721("Experience", "EX") {
+    constructor() ERC721("Experience", "EXP") {
         setPublicChainlinkToken();
         oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
         jobId = "d5270d1c311941d0b08bead21fea7747";
@@ -58,7 +38,9 @@ abstract contract NFTExp is ChainlinkClient, ERC721 {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestAPI() public 
+
+
+    function requesting() public 
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
@@ -75,46 +57,58 @@ abstract contract NFTExp is ChainlinkClient, ERC721 {
         
         // Sends the request
         sendChainlinkRequestTo(oracle, request, fee);
+        counter++;
     }
 
-    function getTokenURI(uint256 tokenId) public view returns (string memory) {
-        return tokenURI(tokenId);
-    }
 
-    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
-        require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
-            "ERC721: transfer caller is not owner nor approved"
-        );
-        setTokenURI(tokenId, _tokenURI);
-    }
-
-   
     /**
      * Callback function
      */
-    function fulfill(bytes32 requestId, bytes32 _timeStarted) public recordChainlinkFulfillment(requestId)
+
+     uint256 public newId;
+     uint256 public counter;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    modifier onlyMinted(){
+        require(counter == 2 ); 
+        _;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) onlyMinted
+        public
+        returns (uint256)
     {
-        timeStarted = _timeStarted; 
-        uint256 newId = experiences.length; 
-        _safeMint(requestToSender[requestId], newId);
+        _tokenIds.increment();
 
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
     }
 
-    function requestNewTime(string memory _newTime) public returns (bytes32) {
-        //require(LINK.balanceOf(address(this) >= fee, "Not enought LINK - fill contract with faucet"));
-       // bytes32 requestId = requestAPI(_time);
-       // requestToNewTime[requestId] = requestId + timeStarted; // TimeSpent 
-       // return requestToNewTime;
-    }
-
-    // function getUser(uint256 _index) external view returns (uint32, string memory, string memory, uint32, bool) {
-    //     (uint32 decodedId, string memory decodedFirstName, string memory decodedLastName, uint32 decodedTimeLeft, bool decodedIsExpired ) = 
-    //         abi.decode(users[_index], (uint32, string, string, uint32, bool)); 
-    //     return (decodedId, decodedFirstName, decodedLastName, decodedTimeLeft, decodedIsExpired);
-    // }
+    function fulfill(bytes32 _requestId, uint256 _realTime) public recordChainlinkFulfillment(_requestId)
+    {
+        if (counter==1){
+            realTime = 0; 
+        }else{
+            realTime = _realTime;
+        }
     
+    }
+
+    function getRealTime(uint256 _realTime) public returns(uint256){
+        realTime = _realTime ;
+    }
+
+    
+
+
+
+
+
+  
 }
-
-
 
